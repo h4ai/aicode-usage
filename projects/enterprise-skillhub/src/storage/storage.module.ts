f0a40622ed25c9343c09ee4d1e7e8bd62c8bd39c
Ubuntu <azureuser@myaivm-tokyo.tail1ab559.ts.net>
2026-03-19 23:33:50 +0800

    fix: StorageModule 补 ConfigModule 导入 + .env 从 example 创建

--- projects/enterprise-skillhub/src/storage/storage.module.ts
+++ projects/enterprise-skillhub/src/storage/storage.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '../config/config.module';
 import { StorageService } from './storage.service';
 
 @Module({
+  imports: [ConfigModule],
   providers: [StorageService],
   exports: [StorageService],
 })
